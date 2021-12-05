50827caed1ea0c351b767fcefcc7eebcffa22ce7
mkuma290 <92900877+mkuma290@users.noreply.github.com>
2021-12-05 22:37:51 +0530

    Add files via upload

---
+++ Customer/src/main/java/com/ondemandcarwash/CustomerApplication.java
@@ -0,0 +1,40 @@
+package com.ondemandcarwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	public static final String CUSTOMER_TAG = "customer service";
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/customer/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.ondemandcarwash"))
+				.build()
+				.tags(new Tag(CUSTOMER_TAG, "the customer API with description api tag"));
+
+	}
+
+}
