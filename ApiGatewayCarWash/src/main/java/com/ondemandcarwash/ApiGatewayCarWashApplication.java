789682090e301e3b3ddeb8e96621d9bbf1379135
mkuma290 <92900877+mkuma290@users.noreply.github.com>
2021-12-05 22:36:32 +0530

    Add files via upload

---
+++ ApiGatewayCarWash/src/main/java/com/ondemandcarwash/ApiGatewayCarWashApplication.java
@@ -0,0 +1,22 @@
+package com.ondemandcarwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayCarWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayCarWashApplication.class, args);
+	}
+	
+	
+	
+
+}
